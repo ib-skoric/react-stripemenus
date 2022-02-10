a218e1c3709f68804b1d3f39045149c3106fea4e
Ivan Branimir Skoric <ib.skoric@outlook.com>
2022-02-10 20:48:25 +0000

    Add structure to the sidebar for mobile version

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 import sublinks from './data'
+import { useGlobalContext } from './context'
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+  const {closeSidebar, isSidebarOpen} = useGlobalContext();
+
+  return(
+       <aside className={`${isSidebarOpen ? "sidebar-wrapper show" : "sidebar-wrapper"}`}>
+         <div className="sidebar">
+           <button className="close-btn" onClick={closeSidebar}>
+             <FaTimes/>
+           </button>
+         </div>
+       </aside>
+    )
 }
 
 export default Sidebar
