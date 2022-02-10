578a41231141d9df4b476e2a024fa68c8a9243c8
Ivan Branimir Skoric <ib.skoric@outlook.com>
2022-02-10 20:27:29 +0000

    Set up context for the app

--- src/context.js
+++ src/context.js
@@ -1,2 +1,40 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({children}) => {
+
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+    
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    }
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    }
+
+    const openSubmenu = () => {
+        setIsModalOpen(true);
+    }
+    const closeSubmenu = () => {
+        setIsSidebarOpen(false);
+    }
+
+    return <AppContext.Provider value={{
+        openSubmenu,
+        closeSubmenu,
+        isSubmenuOpen,
+        openSidebar,
+        closeSidebar,
+        isSidebarOpen
+    }}>
+        {children}
+    </AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+} 
